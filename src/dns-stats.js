ba600b37f4697a22ab18ebb0c67894a3360364a0
valeriapavlova <vapavlovalera@gmail.com>
2025-01-07 00:58:53 +0100

    feat: implement getDNSStats function

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,43 +1,22 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { expect } = require('chai');
+const { getDNSStats } = require('../src/dns-stats.js');
 
-/**
- * Given an array of domains, return the object with the appearances of the DNS.
- *
- * @param {Array} domains
- * @return {Object}
- *
- * @example
- * domains = [
- *  'code.yandex.ru',
- *  'music.yandex.ru',
- *  'yandex.ru'
- * ]
- *
- * The result should be the following:
- * {
- *   '.ru': 3,
- *   '.ru.yandex': 3,
- *   '.ru.yandex.code': 1,
- *   '.ru.yandex.music': 1,
- * }
- *
- */
-function getDNSStats(domains) {
-  const dnsStats = {};
+describe('DNS stats', () => {
+  it('should return domains stats', () => {
+    const domains = [
+      'code.yandex.ru',
+      'music.yandex.ru',
+      'yandex.ru'
+    ];
 
-  domains.forEach(domain => {
-    const parts = domain.split('.').reverse();
-    let key = '';
+    const expected = {
+      '.ru': 3,
+      '.ru.yandex': 3,
+      '.ru.yandex.code': 1,
+      '.ru.yandex.music': 1,
+    };
 
-    parts.forEach(part => {
-      key += `.${part}`;
-      dnsStats[key] = (dnsStats[key] || 0) + 1;
-    });
+    const result = getDNSStats(domains);
+    expect(result).to.deep.equal(expected);
   });
-
-  return dnsStats;
-}
-
-module.exports = {
-  getDNSStats
-};
+});
