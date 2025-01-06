bf0e6028ea4b06ba8c205f9908ec003a820f4e9c
valeriapavlova <vapavlovalera@gmail.com>
2025-01-06 23:38:47 +0100

    fix: improve date validation in getSeason function

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (!(date instanceof Date) || Object.prototype.toString.call(date) !== '[object Date]' || isNaN(date.getTime()) || Object.getOwnPropertyNames(date).length > 0) {
+    throw new Error('Invalid date!');
+  }
+
+  const month = date.getMonth();
+  if (month < 2 || month === 11) return 'winter';
+  if (month < 5) return 'spring';
+  if (month < 8) return 'summer';
+  return 'autumn';
 }
 
 module.exports = {
