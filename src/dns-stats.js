68b3d98f4e125ecf5e733f895a1c85eb59ac39f1
valeriapavlova <vapavlovalera@gmail.com>
2025-01-07 00:55:45 +0100

    feat: implement getDNSStats function

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const dnsStats = {};
+
+  domains.forEach(domain => {
+    const parts = domain.split('.').reverse();
+    let key = '';
+
+    parts.forEach(part => {
+      key += `.${part}`;
+      dnsStats[key] = (dnsStats[key] || 0) + 1;
+    });
+  });
+
+  return dnsStats;
 }
 
 module.exports = {
